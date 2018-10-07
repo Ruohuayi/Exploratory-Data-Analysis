7ef15f0b189d5c4f95331ff9fd22331994a40c1f
Ruohuayi <41515341+Ruohuayi@users.noreply.github.com>
2018-10-07 12:13:02 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,15 +1,13 @@
 #set working directory
 setwd("c:/Users/yii/Desktop/R code/")
 
-#download the .zip file
+#download and unzip the .zip file
 fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 destUrl<-"c:/Users/yii/Desktop/R code/power.zip"
 download.file(fileUrl,destUrl)
-
-#unzip the file
 unzip("power.zip") 
 
-#read the file into R and setup column names
+#read the file into R
 file="household_power_consumption.txt"
 power<-read.table(file=file, sep = ";",header=TRUE, na.strings = '?',colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 power$Date <- as.Date(power$Date, "%d/%m/%Y")
